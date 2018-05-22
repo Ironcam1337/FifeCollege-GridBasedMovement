33abe03ade6cd47e7729e86be035f13af4356c9c
Ironcam1337 <36261440+Ironcam1337@users.noreply.github.com>
2018-05-22 12:39:21 +0100

    Added m_type

--- GridObject.h
+++ GridObject.h
@@ -15,16 +15,24 @@ class GridObject : public GameObject
 {
 
 public:
+	
+	enum Type { PLAYER, DIAMOND, BOULDER, DIRT, EXIT };
+	GridObject(Type _type): m_type(_type) {}
 
 	// GridObject Functions
 	virtual void SetGrid(Grid* _grid) { m_grid = _grid; }
 	virtual void SetGridCoords(int _x, int _y) { m_gridX = _x; m_gridY = _y; }
+	
+	
+	void SetType(Type _newType) { m_type = _newType; }
+	Type GetType() {return m_type;}
+
 
 	void ClearReceivedInput() { m_receivedInput = false; }
 	bool HasRececeivedInput() { return m_receivedInput; }
 
 protected:
-
+	int m_type;
 	bool m_receivedInput;
 	Grid* m_grid;
 	int m_gridX;
